87828edcd7935738072def7fac40bbe69298a2b2
jackwrfuller <jack.fuller101@gmail.com>
2025-09-09 10:55:40 +1000

    add cors middleware

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 
 	s := &http.Server{
 		Addr: ":3000",
-		Handler: router,
+		Handler: corsMiddleware(router),
 	}
 
 	fmt.Println("Starting server...")
@@ -24,4 +24,17 @@ func main() {
 		panic(err)
 	}
 
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*") 
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+        if r.Method == "OPTIONS" {
+            w.WriteHeader(http.StatusOK)
+            return
+        }
+        next.ServeHTTP(w, r)
+    })
 }
