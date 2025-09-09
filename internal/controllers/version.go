e173ff5e262ea238ace8918a48dbc4cc9497f205
jackwrfuller <jack.fuller101@gmail.com>
2025-09-09 10:01:12 +1000

    fix version

--- internal/controllers/version.go
+++ internal/controllers/version.go
@@ -1,10 +1,19 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
+type Version struct {
+	Version string `json:"version"`
+}
+
+var version = Version{
+		Version: "dev",
+}
+
 func (h *BaseHandler) getVersion(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte("Temp Handler dev"))
+	json.NewEncoder(w).Encode(version)
 }
